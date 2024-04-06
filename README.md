2e39a433207fcdd99e156167dfa8880cbe9ae067
Geeobarros <ge.barros96@gmail.com>
2024-04-05 23:18:47 -0300

    crud

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ CRUD
 
 Projeto realizado utilizando o framework Express.js do Node.js
 
+1: npm init - Criará um arquivo package.json
+2: npm express - Instala o módulo express - node_modules
+
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const PORT = 3333;
+app.use(express.json());
+
+//Middleware para logar todas as chamadas
+
+app.use((req, res, next) =>{
+    console.log(`Solicitação recebida de: ${req.method} - ${req.originalUrl}`)
+
+    next();
+})
+
+
+const produtos = []
+//Criando um produto com POST
+app.post('/produto', (req, res) => { //usando try para manipulação de erros
+    try {
+        const produto = req.body;
+        produto.id = produtos.length + 1
+        produtos.push(produto)
+        res.status(201).send('Produto adicionado com sucesso')
+    } catch (error) {
+        res.status(400).send(error)        
+    }
+
+});
+
+//rota GET para retornar os produtos do JSON
+app.get('/produto', (req, res) =>{
+    res.json(produtos)
+})
+ 
+//Rota PUT para atualizar um produto existente
+app.put('/produto/:id', (req, res) =>{
+    try {
+        const { id } = req.params
+        const alteraProduto = req.body
+        const index = produtos.findIndex(prod => prod.id === parseInt(id)) //procura o produto pelo id
+        if (index === -1) {
+            throw new Error('ID do produto não encontrado')           
+            
+        }else {
+            produtos[index] = {...produtos[index], ...alteraProduto}
+            res.status(200).send(`Produto atualizado: Id: ${produtos[index].id} - Nome: ${produtos[index].produto} - Preço: ${produtos[index].preco} - Descrição: ${produtos[index].descricao}`)
+        }
+    } catch (error) {
+        res.status(400).send("Not found" + error)
+    }
+
+    //Rota DELETE para deletar produto 
+    app.delete('/produto/:id', (req, res) => {
+        try {
+            const { id } = req.params
+            const index = produtos.findIndex(prod => prod.id === parseInt(id)) //procura o produto pelo id 
+            
+            if (index === -1){
+                throw new Error ('ID não encontrado')
+            } else {
+                produtos.splice(index, 1)
+                res.status(200).send('Produto excluído com sucesso')
+            }
+            
+        } catch (error) {
+            res.status(400).send("Not found" + error)            
+        }
+
+    })
+})
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server online in http://localhost:${PORT}`)
+});
+
